fe7de2db3df9556e4006a8548ffeba9c350e754f
mesandhya2403 <76815633+mesandhya2403@users.noreply.github.com>
2021-11-27 20:41:57 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,22 @@
+var person1 = {
+    FirstName:'Sandhya',
+    LastName:'Kumari',
+    OfficeAddress:'Patna, Bihar'
+}
+
+var person2 = {
+    FirstName:'Shreya',
+    LastName:'Bharti',
+    OfficeAddress:'Bhagalpur, Bihar'
+}
+
+var person3 = {
+    FirstName:'Shalini',
+    LastName:'Singh',
+    OfficeAddress:'Nagpur, Maharastra'
+}
+
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
